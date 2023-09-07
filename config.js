00a922ef35db5c1e0b2af397e4f41d77c3dec4af
Tomasz <80126476+TOMEOv17@users.noreply.github.com>
2023-09-07 23:58:55 +0200

    news add, news del, admin login

---
+++ config.js
@@ -0,0 +1,5 @@
+module.exports = {
+    keySession: ['TWOJKLUCZ'],
+    maxAgeSession: 24 * 60 * 60 * 1000,
+    db: 'mongodb+srv://<user>:<password>@cluster0.89agedd.mongodb.net/test'
+}
--- routes/admin.js
+++ routes/admin.js
@@ -1,9 +1,51 @@
 const express = require('express');
 const router = express.Router();
+const News = require('../models/news')
 
-/* GET home page. */
-router.get('/', (req, res, next) => {
-  res.render('admin', { title: 'Admin' });
+router.all('*', (req, res, next) => {
+  if(!req.session.admin){
+    res.redirect('/login')
+    return
+  }
+
+  next()
+})
+
+
+async function getNews() {
+  const news = await News.find({});
+  return news;
+}
+router.get('/', (req, res) => {
+  getNews().then(function(data){
+    res.render('admin/index', { title: 'Admin', data });
+  });
+});
+
+
+
+router.get('/news/add', (req, res, next) => {
+  res.render('admin/news_form', { title: 'Dodaj newsa' });
+});
+
+router.post('/news/add', (req, res) => {
+  const body = req.body
+
+  const newsData = new News(body)
+
+  const errors = newsData.validateSync()
+
+  newsData.save()
+  res.redirect('/admin')
+  res.render('admin/news_form', { title: 'Formularz', errors });
+})
+
+async function delNews(id) {
+  await News.findByIdAndDelete(id)
+}
+router.get('/news/delete/:id', (req, res) => {
+  delNews(req.params.id)
+  res.redirect('/admin')
 });
 
 module.exports = router;
