99c1a6f52d5a3f40b254611c8bd765e1440bd558
Tomasz <80126476+TOMEOv17@users.noreply.github.com>
2023-09-09 01:56:24 +0200

    quiz

--- routes/quiz.js
+++ routes/quiz.js
@@ -1,9 +1,29 @@
 const express = require('express');
 const router = express.Router();
+const Quiz = require('../models/quiz')
 
 /* GET home page. */
 router.get('/', (req, res, next) => {
-  res.render('quiz', { title: 'Quiz' });
+  const show = !req.session.vote
+  Quiz.find({})
+  .then(data => {
+    let sum = 0
+    data.forEach(elem => {
+      sum += elem.vote
+    })
+    res.render('quiz', { title: 'Quiz', data, show, sum })
+  }).catch(err => console.log(err))
+});
+
+router.post('/', (req, res) => {
+  const id = req.body.quiz
+  Quiz.findOne({_id: id}).then(item => {
+    item.vote = item.vote + 1
+    item.save()
+  }).then(()=>{
+    req.session.vote = 1
+    res.redirect('/quiz')
+  }).catch(err => console.log(err))
 });
 
 module.exports = router;
